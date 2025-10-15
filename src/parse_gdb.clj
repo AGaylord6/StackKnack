198e4af89594ef8c2bb03d762c9add8929639e63
Andrew Gaylord <agaylord@nd.edu>
2025-10-15 14:39:47 -0400

    Update parsing and remove bloat from repo

--- src/parse_gdb.clj
+++ src/parse_gdb.clj
@@ -31,18 +31,32 @@
 (defn parse-frame [text]
   ;; Parses output of "info frame n"
   ;; Example key lines:
+  ;; Stack level 0, frame at 0x7fffffffe1f0:
   ;; rip = 0x55555555467d in main (main.c:5); saved rip = 0x7ffff7a05290
   ;; Saved registers:
   ;;   rbp at 0x7fffffffe1e0, rip at 0x7fffffffe1e8
   (let [lines (str/split-lines text)
+        ;; Extract frame starting address
+        frame-at-line (some #(re-find #"frame at (0x[0-9a-fA-F]+)" %) lines)
+        ;; Extract current rip info
         rip-line (some #(re-find #"rip = (0x[0-9a-fA-F]+) in (\S+) \(([^:]+):(\d+)\)" %) lines)
-        rbp-line (some #(re-find #"rbp at (0x[0-9a-fA-F]+)" %) lines)]
+        ;; Parse saved registers to create address-to-label mappings
+        saved-regs-section (drop-while #(not (str/includes? % "Saved registers:")) lines)
+        saved-regs (when (seq saved-regs-section)
+                     (->> (rest saved-regs-section)
+                          (take-while #(str/starts-with? % "  "))
+                          (mapcat (fn [line]
+                                    ;; Extract all "register at address" pairs from the line
+                                    (re-seq #"(\w+) at (0x[0-9a-fA-F]+)" line)))
+                          (map (fn [[_ reg addr]] [addr reg]))
+                          (into {})))]
     (cond-> {}
+      frame-at-line (assoc :frame-address (nth frame-at-line 1))
       rip-line (assoc :rip (nth rip-line 1)
                       :function (nth rip-line 2)
                       :file (nth rip-line 3)
                       :line (Integer/parseInt (nth rip-line 4)))
-      rbp-line (assoc :rbp (nth rbp-line 1))
+      saved-regs (assoc :saved-register-mappings saved-regs)
       :always (assoc :raw text))))
 
 (defn parse-registers [text]
